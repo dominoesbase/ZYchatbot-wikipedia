ca5688e9d96c1afc3cd80cf90b855dd78388c4d1
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2017-03-03 18:24:40 +0000

    English Version
    Improvements implemented for the English version.

--- app/controllers/assistant_controller.rb
+++ app/controllers/assistant_controller.rb
@@ -1,12 +1,13 @@
+# Basic controller to respond
 class AssistantController < ApplicationController
   def messenger
     query = (params[:result][:parameters]['any'].present?)? params[:result][:parameters]['any'] : "Ruby On Rails"
     page = WikipediaConsulter.find_by_name(query)
 
     if page.text.present?
-      text = "#{page.text.slice(0, 150)}... \b link completo: #{page.fullurl}"
+      text = "#{page.text.slice(0, 300)}"
     else
-      text = "No encontrei essa pgina na Wikipedia em ingls :("
+      text = "I'm sorry, but I could not find this information."
     end
 
     response =
--- app/services/wikipedia_consulter.rb
+++ app/services/wikipedia_consulter.rb
@@ -1,8 +1,9 @@
+# Create a service that will consult Wikipedia
 class WikipediaConsulter
   require 'wikipedia'
- 
+
   def self.find_by_name(param)
     Wikipedia.find(param)
   end
- 
+
 end
